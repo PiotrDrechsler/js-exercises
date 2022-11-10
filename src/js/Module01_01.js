05c127a9ef65f389479f48090e8bd1dbb910ab4d
PiotrDrechsler <pdrech@gmail.com>
2022-11-10 13:27:38 +0100

    upd1
    upd`

---
+++ src/js/Module01_01.js
@@ -0,0 +1,112 @@
+/*
+//01
+let productName = 'Droid';
+let pricePerItem = 2000;
+
+//02
+productName = 'Repair Droid';
+pricePerItem = 3500;
+
+console.log(productName);
+console.log(pricePerItem);
+
+//03
+const topSpeed = 160;
+console.log(topSpeed);
+
+const distance = 617.54;
+console.log(distance);
+
+const login = 'mango935';
+console.log(login);
+
+const isOnline = true;
+console.log(isOnline);
+
+const isAdmin = false;
+console.log(isAdmin);
+
+//04
+const pricePerItem = 3500;
+const orderedQuantity = 4;
+
+const totalPrice = pricePerItem * orderedQuantity;
+console.log(totalPrice);
+
+//05
+const productName = "Droid";
+const pricePerItem = 3500;
+
+const message = `You picked ${productName}, price per item is ${pricePerItem} credits`;
+console.log(message);
+
+//06
+const pricePerDroid = 800;
+const orderedQuantity = 6;
+const deliveryFee = 50;
+const totalPrice = pricePerDroid * orderedQuantity + deliveryFee;
+const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`
+console.log(message);
+
+//07
+function sayHi() {
+    console.log("Hello, this is my first function!");
+}
+
+sayHi();
+
+//08
+function add(a, b, c) {
+    console.log(`Addition result equals ${a+b+c}`);
+}
+  
+add(15, 27, 10);
+add(10, 20, 30);
+add(5, 10, 15);
+
+//09
+function add(a, b, c) {
+   return a + b + c;
+};
+  
+add(2, 5, 8); // 15
+  
+console.log(add(15, 27, 10));
+console.log(add(10, 20, 30));
+console.log(add(5, 10, 15));
+
+//10
+function makeMessage (name, price) {
+
+    const message = `You picked ${name}, price per item is ${price} credits`;
+
+    return message;
+};
+
+console.log(makeMessage(`Paula`, 69));
+
+//11
+function calculateTotalPrice (orderedQuantity, pricePerItem) {
+    
+    const totalPrice = orderedQuantity * pricePerItem;
+
+    return totalPrice;
+};
+
+console.log(calculateTotalPrice(5,666));
+ 
+//12
+function makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) {
+    
+    const totalPrice = orderedQuantity * pricePerDroid + deliveryFee;
+
+    const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
+  
+    return message;
+};
+
+console.log(makeOrderMessage(6,666,69));
+*/
+
+//13
+gfdtrdrgd
